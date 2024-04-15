7794027f1844185c4eff91643b6fbd30930761fd
Jeff Matthew Dallo <jeffmatthew.dallo@letran.edu.ph>
2024-04-15 22:30:18 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from 'src/basic-highlight/basic-higlight.directive';
+import { BetterHighlightDirective } from '../basic-highlight/better-highlight.directive';
+import { UnlessDirective } from 'src/basic-highlight/UnlessDirective';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    UnlessDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
